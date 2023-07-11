c6549cf7d8ca2c10d9c96dc4b29b93776b0b6da1
Manu <manu.angot@hotmail.fr>
2023-07-11 10:29:20 +0200

    ajout du commtcontroller

---
+++ back/controllers/commentController.py
@@ -0,0 +1,41 @@
+from sqlalchemy.orm import Session
+from sqlalchemy import Select
+from bdd import get_db
+from models import Commentaire, engine
+
+class commentController():
+    def __init__(self, db: Session = Depends(get_db)):
+        self.db = db
+
+    def create_commentaire(self, commentaire: Commentaire):
+        self.db.add(commentaire)
+        self.db.commit()
+        self.db.refresh(commentaire)
+        return commentaire
+
+    def get_commentaire(self, commentaire_id: int):
+        commentaire = self.db.query(Commentaire).filter(Commentaire.id == commentaire_id).first()
+        if not commentaire:
+            raise HTTPException(status_code=404, detail="Commentaire not found")
+        return commentaire
+
+    def update_commentaire(self, commentaire_id: int, updated_commentaire: Commentaire):
+        commentaire = self.db.query(Commentaire).filter(Commentaire.id == commentaire_id).first()
+        if not commentaire:
+            raise HTTPException(status_code=404, detail="Commentaire not found")
+        commentaire.content = updated_commentaire.content
+        self.db.commit()
+        self.db.refresh(commentaire)
+        return commentaire
+
+    def delete_commentaire(self, commentaire_id: int):
+        commentaire = self.db.query(Commentaire).filter(Commentaire.id == commentaire_id).first()
+        if not commentaire:
+            raise HTTPException(status_code=404, detail="Commentaire not found")
+        self.db.delete(commentaire)
+        self.db.commit()
+        return {"message": "Commentaire deleted"}
+
+    def get_all_commentaires(self):
+        commentaires = self.db.query(Commentaire).all()
+        return commentaires
--- back/controllers/userController.py
+++ back/controllers/userController.py
@@ -1,8 +1,16 @@
-def get_users():
-    # Code pour récupérer la liste de tous les utilisateurs
-    return 'Liste des utilisateurs'
+from sqlalchemy.orm import Session
+from sqlalchemy import Select
+from models import User, engine
 
-def get_user(user_id):
-    # Code pour récupérer un utilisateur à partir de son Id
-    return 'Utilisateur avec Id : ()'.format(user_id)
+class UserController():
 
+    def get_users():
+        # Code pour récupérer la liste de tous les utilisateurs
+        return 'Liste des utilisateurs'
+
+    def get_user(user_id):
+        # Code pour récupérer un utilisateur à partir de son Id
+        return 'Utilisateur avec Id : ()'.format(user_id)
+
+    def login():
+        pass
