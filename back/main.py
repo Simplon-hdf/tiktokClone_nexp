8fae131bae919145ce9b8af321075e8e75c55e16
Pierre Verdiere <pverdiere59@gmail.com>
2023-07-11 16:19:49 +0200

    feat: add cors middleware

--- back/main.py
+++ back/main.py
@@ -2,9 +2,15 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from models import engine, Base
 from routes import user_router, video_router, commentaire_router
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"]
+)
 
 app.include_router(user_router)
 app.include_router(video_router)
