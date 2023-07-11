a3f2fca2d169b7f1677cc85725cd4ca26517c7b0
Pierre Verdiere <pverdiere59@gmail.com>
2023-07-11 15:27:25 +0200

    feat: add upload_video

--- back/controllers/videoController.py
+++ back/controllers/videoController.py
@@ -1,6 +1,8 @@
 from sqlalchemy.orm import Session
 from sqlalchemy import Select
-from models import User, engine
+from models import Video, engine
+from fastapi import File, UploadFile
+import uuid
 
 class VideoController():
 
@@ -8,4 +10,16 @@ class VideoController():
         # Code pour récupérer la liste de toutes les videos
         return 'Liste des videos'
 
- 
+    def upload_video(data:dict):
+        file: UploadFile = File(data["file"])
+        file.filename = uuid.uuid4()
+        try:
+            with open(file.filename, "wb") as f:
+                while contents := file.file.read(1024 * 1024):
+                    f.write(contents)
+        except Exception:
+            return {"message": "There was an error uploading the file"}
+        finally:
+            file.file.close()
+
+        return {"message": f"Successfully uploaded {file.filename}"}
