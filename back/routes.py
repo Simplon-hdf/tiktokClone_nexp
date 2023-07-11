4b1fe3eeecfec4ad5b1440c895834cd6666bc3c1
Gaston59400 <doroff59@gmail.com>
2023-07-11 09:33:35 +0200

    Structured folders for controllers

--- back/routes.py
+++ back/routes.py
@@ -1,28 +1,40 @@
 from fastapi import APIRouter
-from controlers import user_controller, video_controller, commentaire_controller
+from back.controllers.userController import user_controller, video_controller, commentaire_controller
 
 user_router = APIRouter()
 video_router = APIRouter()
 commentaire_router = APIRouter()
+tags_router = APIRouter()
 
 
 # Utilisateurs
 @user_router.post('/users/login')
-#@user_router.post('/users/signup')
+@user_router.post('/users/signup')
+@user_router.post('/users/create')
+@user_router.get('/users/list')
+def get_users():
+    return user_controller.get_users()
 @user_router.put('/users/update')
 @user_router.delete('/users/delete')
 
-@user_router.get('/users')
-def get_users():
-    return user_controller.get_users()
-
 # Vidéos
+@video_router.post('/videos/create')
+@video_router.get('/videos/list')
 @video_router.put('/videos/update')
 @video_router.delete('/videos/delete')
-@video_router.get('/videos')
+
 
 # Commentaires
+@commentaire_router.post('/commentaires/create')
+@commentaire_router.get('/commentaires/list')
 @commentaire_router.put('/commentaires/update')
 @commentaire_router.delete('/commentaires/delete')
-@commentaire_router.get('/commentaires')
+
+# Tags
+@tags_router.post('/tags/create')
+@tags_router.get('/tags/list')
+@tags_router.put('/tags/update')
+@tags_router.delete('/tags/delete')
+
+
 
