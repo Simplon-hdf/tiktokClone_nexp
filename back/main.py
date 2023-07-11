16c4a4c8f42395f883f70e35880628d6d025d435
Gaston59400 <doroff59@gmail.com>
2023-07-11 09:09:38 +0200

    Started create routes and controllers

--- back/main.py
+++ back/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from models import engine, Base
+from routes import user_router, video_router, commentaire_router
+
 
 app = FastAPI()
 
+app.include_router(user_router)
+app.include_router(video_router)
+app.include_router(commentaire_router)
+
+
 # Créez la table dans la base de données
 Base.metadata.create_all(bind=engine)
+
