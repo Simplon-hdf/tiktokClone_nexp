7d98016f30bb150f89e9aa1e89a6788162a06ae0
Gaston59400 <doroff59@gmail.com>
2023-07-11 10:26:34 +0200

    Update Folder Controllers

---
+++ back/controllers/tagController.py
@@ -0,0 +1,11 @@
+from sqlalchemy.orm import Session
+from sqlalchemy import Select
+from models import User, engine
+
+class TagController():
+
+    def get_tags():
+        # Code pour récupérer la liste de tous les tags
+        return 'Liste des tags'
+
+ 
