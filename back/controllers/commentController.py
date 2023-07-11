038c2c4363085db849fbbf1b65087782aafb0186
Manu <manu.angot@hotmail.fr>
2023-07-11 11:52:36 +0200

    CommentController est mis à jour

--- back/controllers/commentController.py
+++ back/controllers/commentController.py
@@ -1,41 +1,48 @@
 from sqlalchemy.orm import Session
 from sqlalchemy import Select
-from bdd import get_db
 from models import Commentaire, engine
 
 class CommentController():
-    def __init__(self, db: Session = Depends(get_db)):
-        self.db = db
-
+    
     def create_commentaire(self, commentaire: Commentaire):
-        self.db.add(commentaire)
-        self.db.commit()
-        self.db.refresh(commentaire)
+       with Session(engine) as session:
+            newCommentaire = Commentaire(
+                content = data["content"],
+                created_at = data["created_at"],
+                updated_at = data["updated_at"],
+                idVideo = data["idVideo"],
+                idUser = data["idUser"]       
+                )
+            )
+            session.add(newCommentaire)
+            session.commit()
         return commentaire
 
+     def get_commentaires(self):
+        commentaires = session.query(Commentaire).all()
+        return 'Liste des commentaires'
+
     def get_commentaire(self, commentaire_id: int):
-        commentaire = self.db.query(Commentaire).filter(Commentaire.id == commentaire_id).first()
+        commentaire = session.query(Commentaire).filter(Commentaire.id == commentaire_id).first()
         if not commentaire:
             raise HTTPException(status_code=404, detail="Commentaire not found")
         return commentaire
 
     def update_commentaire(self, commentaire_id: int, updated_commentaire: Commentaire):
-        commentaire = self.db.query(Commentaire).filter(Commentaire.id == commentaire_id).first()
+        commentaire = session.query(Commentaire).filter(Commentaire.id == commentaire_id).first()
         if not commentaire:
             raise HTTPException(status_code=404, detail="Commentaire not found")
         commentaire.content = updated_commentaire.content
-        self.db.commit()
-        self.db.refresh(commentaire)
+        session.commit()
+        session.refresh(commentaire)
         return commentaire
 
     def delete_commentaire(self, commentaire_id: int):
-        commentaire = self.db.query(Commentaire).filter(Commentaire.id == commentaire_id).first()
+        commentaire = session.query(Commentaire).filter(Commentaire.id == commentaire_id).first()
         if not commentaire:
             raise HTTPException(status_code=404, detail="Commentaire not found")
-        self.db.delete(commentaire)
-        self.db.commit()
+        session.delete(commentaire)
+        session.commit()
         return {"message": "Commentaire deleted"}
 
-    def get_all_commentaires(self):
-        commentaires = self.db.query(Commentaire).all()
-        return commentaires
+   
