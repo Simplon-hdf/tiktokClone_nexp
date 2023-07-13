853d5abe65cd2b398b916ec9f48fa82830aaca34
Gaston59400 <doroff59@gmail.com>
2023-07-13 11:09:16 +0200

    Finalized Users Controllers

--- back/controllers/userController.py
+++ back/controllers/userController.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy import select
 from models import User, engine
 from argon2 import PasswordHasher
 from jose import jwt
@@ -57,7 +58,41 @@ class UserController():
             except SQLAlchemyError as e:
                 session.rollback()
                 raise HTTPException(status_code=500, detail="Erreur de création de l'utilisateur")
+    
+    def put_users(self, data: dict):
+        print(data)
+        try:
+            with Session(engine) as session:
+                db_user = session.query(User).filter(User.id == data["id"]).first()
 
+                if not db_user:
+                    return None
+
+                # Mettre à jour les champs de l'utilisateur
+                if 'email' in data:
+                    db_user.email = data['email']
+                if 'pseudo' in data:
+                    db_user.pseudo = data['pseudo']
+                if 'password' in data:
+                    db_user.password = data['password']
+
+                # Enregistrer les modifications dans la base de données
+                session.commit()
+                session.refresh(db_user)
+
+                return db_user
+
+        except SQLAlchemyError as e:
+            session.rollback()
+            raise HTTPException(status_code=500, detail='Erreur de base de données') from e
+
+    def delete_user(self, id: int) -> None:
+        session = Session(engine)
+        query = select(User).where(User.id.in_([id]))
+        for user in session.scalars(query):
+            session.delete(user)
+            session.commit()
+    
     def generate_jwt_token(self, user_data: User):
         user_id = user_data.id
         user_email = user_data.email
